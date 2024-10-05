cd3a762c4065a7726f98cdb09ed0c39a83341fb0
bill donner <billdonner@gmail.com>
2024-10-05 08:57:53 -0500

    Initial Commit

---
+++ dmangler/dmanglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dmanglerApp.swift
+//  dmangler
+//
+//  Created by bill donner on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct dmanglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
